c70bd8a45d17de3ecef3aa739cc4fffb4198f347
Angel Herrarte <aherrarte-2019037@kinal.edu.gt>
2020-10-07 13:56:35 -0600

    Se creo el proyecto en angular

---
+++ app-basica-angular/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './main/app.component';
+import { SaludarComponent } from './saludar/saludar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SaludarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
